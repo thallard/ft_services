0bd3e0a71484a34d534dd48a05badc29c30a02b8
thallard <thallard@student.42lyon.fr>
2021-02-11 15:58:27 +0100

    Initial

--- srcs/nginx/setup.sh
+++
@@ -1,7 +0,0 @@
-
-rm -rf /etc/nginx/sites-enabled/default
-cp nginx.conf /etc/nginx/sites-enabled/
-
-nginx -g 'pid /tmp/nginx.pid; daemon off;'
-
-tail -f /var/log/nginx/access.log /var/log/nginx/error.log
